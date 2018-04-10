386245f6dbafea57f88776a9fe700a9d561d9377
pekape <37801685+pekape@users.noreply.github.com>
2018-04-10 14:24:24 +0300

    3.4

--- index.js
+++ index.js
@@ -19,12 +19,18 @@ app.get('/api/persons/:id', (req, res) => {
   res.status(404).end()
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log('bzzzzz')
 })
 
-const persons = [
+let persons = [
   {
     name: "Arto Hellas",
     number: "040-123456",
